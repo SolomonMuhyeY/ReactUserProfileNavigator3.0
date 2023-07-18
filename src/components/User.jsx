6763c50cccfb2d09377d53e6db0aca28a687e551
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-07-19 01:34:51 +0300

    finishing up user component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,15 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Box, Button, Typography } from "@mui/material";
+
 export default function User({ client }) {
   return (
-    <div className='rounded shadow-md ring-offset-2 ring-1 ring-gray-300 m-4 mx-auto text-center py-2 px-4 w-fit transition duration-1500 ease hover:scale-105'>
+    <Box
+      sx={{
+        borderRadius: "10px",
+        py: "0.5rem",
+        px: "0.85rem",
+        my: "16px",
+        mx: "auto",
+        textAlign: "center",
+        boxShadow: "2px 0 1px 0 #ddd, -2px 0 1px 0 #ddd, 0 2px 1px 0 #ddd",
+        ":hover": {
+          scale: "1.05",
+          transition: "ease-in-out",
+          transitionDuration: "2000ms",
+        },
+      }}
+    >
       <img src={client.img} />
-      <p className='font-bold italic'>{client.name}</p>
+      <Typography
+        variant='h6'
+        sx={{ fontStyle: "italic", fontWeight: "bolder" }}
+      >
+        {client.name}
+      </Typography>
       <Link to={`/UserDescription/${client.id}`}>
-        <button className='bg-blue-600 p-1 my-3 rounded transition duration-1000 ease  hover:bg-blue-500'>
+        <Button
+          variant='contained'
+          color='primary'
+          sx={{
+            py: "3px",
+            px: "5px",
+            fontSize: "small",
+            wordSpacing: "-2px",
+            borderRadius: "5px",
+            ":hover": {
+              bgcolor: "rgb(32, 32, 210)",
+            },
+            transition: "all",
+            transitionDuration: "2000ms",
+            transition: "ease-in-out",
+            my: "0.56rem",
+          }}
+          className='bg-blue-600 p-1 my-3 rounded transition duration-1000 ease  hover:bg-blue-500'
+        >
           {client.btnMsg}
-        </button>
+        </Button>
       </Link>
-    </div>
+    </Box>
   );
 }
