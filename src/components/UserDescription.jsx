f5d98d8cd4fad5ec8e32fc22d0f8cb472caaaf51
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-07-19 02:44:08 +0300

    finishing up UserDecription.jsx component

--- src/components/UserDescription.jsx
+++ src/components/UserDescription.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { Button, Container, Typography } from "@mui/material";
 import { client } from "./UserInfo";
 
 function UserDescription() {
@@ -11,20 +12,28 @@ function UserDescription() {
     navigate(-1);
   };
   if (!user) {
-    return <div className='font-bold italic'>User not found</div>;
+    return <div>User not found</div>;
   }
   return (
-    <div className='flex-wrap max-w-screen-md overflow-auto overflow-x-hidden text-left md:w-600'>
-      <img className='text-left mr-auto' src={user.img} alt={user.name} />
-      <h2 className='text-2xl font-bold italic m-2'>{user.name}</h2>
-      <p className='text-left'>{user.description}</p>
-      <button
-        className='bg-blue-500 rounded text-white py-2 px-5 mt-5 transition duration-300 cursor-pointer hover:bg-blue-400 hover:text-black'
+    <Container>
+      <img src={user.img} alt={user.name} />
+      <Typography
+        variant='h5'
+        sx={{ fontWeight: "bolder", fontStyle: "italic" }}
+      >
+        {user.name}
+      </Typography>
+      <Typography variant='body1'>{user.description}</Typography>
+      <Button
+        variant='contained'
+        sx={{
+          my: ".44rem",
+        }}
         onClick={handleGoBack}
       >
         Go Back
-      </button>
-    </div>
+      </Button>
+    </Container>
   );
 }
 
